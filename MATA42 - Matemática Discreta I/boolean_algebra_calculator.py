7fa6e4da5b6c34a2cae8e96ae3f368b92d4cc983
JeanExtreme002 <jeangamerextreme@gmail.com>
2021-08-19 00:23:28 -0300

    refactored code

--- MATA42 - Matemática Discreta I/boolean_algebra_calculator.py
+++ MATA42 - Matemática Discreta I/boolean_algebra_calculator.py
@@ -1,28 +1,49 @@
 __author__ = "Jean Loui Bernard Silva de Jesus"
 
-def find_encapsulated_expressions(expression):
-    expressions = []
-    index = 0
-    
-    while index < len(expression):
-        char = expression[index]
-        
-        if char == "(":
-            parentheses_count = 1
-            for next_index in range(index + 1, len(expression)):
-                if expression[next_index] == "(": parentheses_count += 1
-                if expression[next_index] == ")": parentheses_count -= 1
-                if parentheses_count == 0: break
-            expressions.append([index, next_index + 1])
-            index = next_index
-        index += 1
-    return expressions
+def find_closing_parenthesis(index, string):
+    """
+    Recebe a posição do parêntese de abertura e retorna
+    a posição do seu respectivo parêntese de fechamento.
+    """
+    count = 1
 
-def get_logic_value(expression):
+    for next_index in range(index + 1, len(string)):
+        if string[index] == "(": count += 1
+        elif string[next_index] == ")": count -= 1
+        if count == 0: return next_index
+    return -1
+
+def find_encapsulated_expressions(expression):
+    """
+    Função geradora que retorna a posição (start, end) de todas as
+    expressões dentro de parênteses (incluindo seus parênteses).
+    """
+    index = 0
+
+    while index < len(expression):
+        if expression[index] == "(":
+            end = find_closing_parenthesis(index + 1, expression)
+            yield index, end + 1
+            index = end
+        index += 1
+
+def calculate_boolean_expression(expression):
+    """
+    Calcula uma expressão booleana, cujo valor lógico das sentenças
+    que compõem a expressão e do resultado final deve ser 0 ou 1.
+
+    CONECTIVOS LÓGICOS:
+    Negação: (~ ou ¬)
+    Conjunção: (^)
+    Disjunção Inclusiva: (v)
+    Disjunção Exclusiva: (xv)
+    Condicional: (->)
+    Bicondicional: (<->)
+    """
     if "(" in expression:
         for start, end in find_encapsulated_expressions(expression):
             exp = expression[start: end]
-            exp_value = get_logic_value(exp[1: -1])
+            exp_value = calculate_boolean_expression(exp[1: -1])
             expression = expression[:start] + " " * (len(exp) - len(exp_value)) + exp_value + expression[end:]
 
     expression = expression.replace(" ", "").replace("¬", "~").replace("~~", "")
@@ -38,8 +59,7 @@ expression = "(p->q)^(q->p)"
 for i in range(1, -1, -1):
     for x in range(1, -1, -1):
         exp = expression.replace("p", str(i)).replace("q", str(x))
-        print(x, i, get_logic_value(exp), exp)
+        print(x, i, calculate_boolean_expression(exp), exp)
 
 
 # PROJETO EM ANDAMENTO...
-
