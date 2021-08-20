38a3f767a0840cde612ec6acef58b7029c2ea649
JeanExtreme002 <jeangamerextreme@gmail.com>
2021-08-20 19:43:41 -0300

    fixed XOR operator and find_closing_parenthesis function

--- MATA42 - Matemática Discreta I/boolean_algebra_calculator.py
+++ MATA42 - Matemática Discreta I/boolean_algebra_calculator.py
@@ -41,7 +41,7 @@ def find_closing_parenthesis(index, string):
     count = 1
 
     for next_index in range(index + 1, len(string)):
-        if string[index] == "(": count += 1
+        if string[next_index] == "(": count += 1
         elif string[next_index] == ")": count -= 1
         if count == 0: return next_index
     return -1
@@ -66,53 +66,54 @@ def get_expression_sentences(expression):
     ser letras ou palavras, desde que sejam obrigatoriamente minúsculas e
     formadas apenas por letras sem acentos (a-z).
     """
-    sentences = set()
+    sentences = []
 
     # Remove todos os operadores lógicos. Isso é feito para que seja possível
-    # separar as sentenças pelos espaços em branco. Caso isso não fosse feito
-    # haveria problemas para indentificar sentenças que não estão separadas dos
-    # operadores lógicos, como "~(p^q)", por exemplo.
+    # separar as sentenças pelos espaços em branco. Do contrário, haveria
+    # problemas para identificar as sentenças que não estão separadas dos
+    # operadores lógicos ou parênteses, como "~(p^q)", por exemplo.
     for operator in get_logical_operators():
         expression = expression.replace(operator, " ")
 
-    # Separa a expressão pelos espaços em branco e procura pelas sentenças.
+    # Separa a expressão pelos espaços em branco e procura pelas sentenças, verificando
+    # se a substring está dentro do range [ord("a") = 97, ord("z") = 122].
     for substring in expression.split():
-        if all([97 <= ord(char) <= 122 for char in substring]): # 97 = ord("a"), 122 = ord("z")
-            sentences.add(substring)
-    return list(sentences)
+        if not substring in sentences and all([97 <= ord(char) <= 122 for char in substring]):
+            sentences.append(substring)
+    return sentences
 
 def get_logical_operators():
     """
-    Retorna uma lista com todos os operadores lógicos.
+    Retorna uma lista com todos os símbolos de operadores lógicos que podem ser utilizados.
     """
-    return ["(", ")", "~", "¬", "^", "V", "XV", "<->", "->"]
-
+    return ["(", ")", "~", "¬", "^", "XV", "V", "<->", "->"]
 
 if __name__ == "__main__":
-    print("""OPERADORES LÓGICOS:
-- Negação: (~ ou ¬)
-- Conjunção: (^)
-- Disjunção Inclusiva: (V)
-- Disjunção Exclusiva: (XV)
-- Condicional: (->)
-- Bicondicional: (<->)""")
+    # Abre o arquivo de informações do programa e imprime o texto.
+    with open("boolean_algebra_calculator_info.txt", encoding = "utf-8") as info_file:
+        print(info_file.read())
 
-    print("""\nSENTENÇAS: podem ser letras ou palavras, desde que sejam obrigatoriamente
-minúsculas e formadas apenas por letras sem acentos (a-z).\n""")
-
+    # Obtém a expressão a ser calculada e substitui chaves e colchetes por parênteses.
     expression = input("Expressão: ")
+    expression = expression.replace("{", "(").replace("}", ")").replace("[", "(").replace("]", ")")
+
+    # Obtém as sentenças da expressão e todas as suas combinações na tabela verdade.
     sentences = get_expression_sentences(expression)
     truth_table = list(itertools.product((1, 0), repeat = len(sentences)))
 
+    # Imprime o header da tabela.
     print("\nTABELA VERDADE:\n")
-
     print(*sentences, expression, sep = " | ")
     print("-" * (len(sentences) * 4 + len(expression) + 5))
 
+    # Percorre cada linha da tabela verdade.
     for row in range(len(truth_table)):
         new_expression = expression
 
+        # Percorre as colunas da tabela, trocando as sentenças na expressão pelos valores booleanos.
         for column in range(len(sentences)):
             new_expression = new_expression.replace(sentences[column], str(truth_table[row][column]))
+
+        # Calcula e imprime o resultado, junto com os parâmetros usados.
         print(*truth_table[row], calculate_boolean_expression(new_expression), sep = " | ")
     input()
