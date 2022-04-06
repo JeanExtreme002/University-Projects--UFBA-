d61fa620937a81fe05920b316277f66902869611
JeanExtreme002 <jeangamerextreme@gmail.com>
2022-04-06 00:54:02 -0300

    fixed identation

--- MATA40 - Estrutura de Dados e Algoritmos I/Comparing Fibonacci Algorithms/Main.java
+++ MATA40 - Estrutura de Dados e Algoritmos I/Comparing Fibonacci Algorithms/Main.java
@@ -1,24 +1,24 @@
 class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// Verifica se recebeu a posição como argumento.
-		if (args.length < 1) {
-			System.out.println("Informe a posição da sequência de Fibonacci a ser calculada!");
-			return;
-		}
+        // Verifica se recebeu a posição como argumento.
+        if (args.length < 1) {
+            System.out.println("Informe a posição da sequência de Fibonacci a ser calculada!");
+            return;
+        }
 
-		// Obtém as posições do array no tipo inteiro.
-		int position1 = Integer.parseInt(args[0]);
-		int position2 = (args.length > 1) ? Integer.parseInt(args[1]) : position1;
+        // Obtém as posições do array no tipo inteiro.
+        int position1 = Integer.parseInt(args[0]);
+        int position2 = (args.length > 1) ? Integer.parseInt(args[1]) : position1;
 
-		// Calcula o valor na posição N da sequência de Fibonacci usando uma função linear.
-		System.out.println("Resultado para a posição " + position1 + ":");
-		System.out.println("Algoritmo Linear: " + new Linear().run(position1) + " millisegundos.");
+        // Calcula o valor na posição N da sequência de Fibonacci usando uma função linear.
+        System.out.println("Resultado para a posição " + position1 + ":");
+        System.out.println("Algoritmo Linear: " + new Linear().run(position1) + " millisegundos.");
 
-		System.out.println("-".repeat(60));
+        System.out.println("-".repeat(60));
 
-		// Calcula o valor na posição N da sequência de Fibonacci usando uma função quadrática.
-		System.out.println("Resultado para a posição " + position2 + ":");
-		System.out.println("Algoritmo Quadrático: " + new Quadratic().run(position2) + " millisegundos.");
-	}
+        // Calcula o valor na posição N da sequência de Fibonacci usando uma função quadrática.
+        System.out.println("Resultado para a posição " + position2 + ":");
+        System.out.println("Algoritmo Quadrático: " + new Quadratic().run(position2) + " millisegundos.");
+    }
 }
--- MATA40 - Estrutura de Dados e Algoritmos I/Comparing Fibonacci Algorithms/Quadratic.java
+++ MATA40 - Estrutura de Dados e Algoritmos I/Comparing Fibonacci Algorithms/Quadratic.java
@@ -1,12 +1,12 @@
 class Quadratic extends Tester {
-	/**
-	Calcula um valor da sequência de Fibonacci, 
-	dado uma posição, recursivamente.
-	*/
-	public long executable(int position) {
-		if (position <= 2) {
-			return 1;
-		}
-		return this.executable(position - 1) + this.executable(position - 2);
-	}
+    /**
+    Calcula um valor da sequência de Fibonacci, 
+    dado uma posição, recursivamente.
+    */
+    public long executable(int position) {
+        if (position <= 2) {
+            return 1;
+        }
+        return this.executable(position - 1) + this.executable(position - 2);
+    }
 }
