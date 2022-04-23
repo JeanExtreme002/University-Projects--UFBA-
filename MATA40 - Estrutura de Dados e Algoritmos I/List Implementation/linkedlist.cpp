b96d7bacd0814c89c9e5f395b93f1785c09b86e6
JeanExtreme002 <jeangamerextreme@gmail.com>
2022-04-22 23:37:02 -0300

    added move() method

--- MATA40 - Estrutura de Dados e Algoritmos I/List Implementation/linkedlist.cpp
+++ MATA40 - Estrutura de Dados e Algoritmos I/List Implementation/linkedlist.cpp
@@ -2,10 +2,11 @@
 
 /**
 LinkedElement é uma struct utilizada pela classe LinkedList, no qual 
-possuirá o seu próprio conteúdo e o endereço do elemento ao qual está vinculado.
+possuirá o seu próprio conteúdo e o endereço dos elementos vinculados ao mesmo.
 */
 template <typename Type> struct LinkedElement {
     Type content;
+    LinkedElement<Type> *previous;
     LinkedElement<Type> *next;
 };
 
@@ -18,9 +19,13 @@ template <typename ElementType> class LinkedList {
     private:
         const int BEGIN = 0;
         const int END = -1;
+        const int LEFT = 0;
+        const int RIGHT = -1;
+
+        int length = 0;
 
         LinkedElement<ElementType> *firstElement = NULL;
-        int length = 0;
+        LinkedElement<ElementType> *lastElement = NULL;
 
         /**
         Método para criar um novo objeto de LinkedElement, retornando o seu ponteiro.
@@ -29,6 +34,7 @@ template <typename ElementType> class LinkedList {
             struct LinkedElement<ElementType> *newElement = new LinkedElement<ElementType>;
 
             newElement->content = element;
+            newElement->previous = NULL;
             newElement->next = NULL;
 
             return newElement;
@@ -48,6 +54,29 @@ template <typename ElementType> class LinkedList {
             return false;
         }
 
+        void move(int direction) {
+            // Não é possível mover a lista caso haja menos de dois elementos.
+            if (length < 2) {
+                return;
+            }
+
+            struct LinkedElement<ElementType> *start;
+            struct LinkedElement<ElementType> *end;
+
+            // Move a lista para a esquerda ou direita, alterando o elemento inicial e final.
+            if (direction == LEFT) {
+                start = firstElement->next;
+                end = firstElement;
+            } 
+            else {
+                start = lastElement;
+                end = lastElement->previous;
+            }
+
+            firstElement = start;
+            lastElement = end;
+        }
+
     public:
         /**
         Destrutor da classe.
@@ -61,6 +90,28 @@ template <typename ElementType> class LinkedList {
         */
         ElementType operator [](int index) {
             return get(index);
+        }
+
+        /**
+        Método mover a lista N vezes à esquerda.
+        */
+        void operator <<(int steps) {
+            steps %= length;
+
+            for (int i = 0; i < steps; i++) {
+                move(LEFT);
+            }
+        }
+
+        /**
+        Método mover a lista N vezes à direita.
+        */
+        void operator >>(int steps) {
+            steps %= length;
+
+            for (int i = 0; i < steps; i++) {
+                move(RIGHT);
+            }
         }
 
         /**
@@ -130,39 +181,61 @@ template <typename ElementType> class LinkedList {
             // Cria um LinkedElement para o elemento a ser inserido.
             struct LinkedElement<ElementType> *newElement = createLinkedElement(element);
 
-            // Se não houver elemento na lista, o elemento será adicionado como o primeiro.
+            // Se não houver elemento na lista, o elemento será adicionado como 
+            // o primeiro e último da lista.
             if (length++ == 0) {
                 firstElement = newElement;
+                firstElement->previous = newElement;
+                firstElement->next = newElement;
+
+                lastElement = newElement;
+                lastElement->previous = newElement;
+                lastElement->next = newElement;
                 return;
             }
 
             // Caso solicitado a inserção do elemento no início da lista, será necessário
-            // apenas fazê-lo apontar para o atual elemento na primeira posição e trocar o objeto.
+            // apenas vincular o último elemento com o elemento da inserção e o elemento
+            // da inserção com o primeiro elemento. Após isso, o primeiro elemento passará
+            // a ser o elemento da inserção.
             if (index == BEGIN) {
                 newElement->next = firstElement;
+                newElement->previous = lastElement;
+
+                firstElement->previous = newElement;
+                lastElement->next = newElement;
+
                 firstElement = newElement;
                 return;
             }
 
-            // Caso solicitado a inserção do elemento no final da lista, o índice será trocado
-            // pela quantidade de elementos que a lista possui.
+            // Caso solicitado a inserção do elemento no final da lista, será necessário
+            // apenas vincular o último elemento com o elemento da inserção e o elemento
+            // da inserção com o primeiro elemento. Após isso, o último elemento passará
+            // a ser o elemento da inserção.
             if (index == END) {
-                index = length - 1; // Subtrai (+1) porque a inserção já foi computada lá em cima.
+                newElement->next = firstElement;
+                newElement->previous = lastElement;
+
+                lastElement->next = newElement;
+                lastElement = newElement;
+                return;
             }
 
             // Percorre todos os elementos ligados até chegar ao elemento alvo.
-            struct LinkedElement<ElementType> *previousElement = NULL;
             struct LinkedElement<ElementType> *targetElement = firstElement;
 
-            while (--index >= 0) {
-                previousElement = targetElement;
+            for (int i = 0; i < index; i++) {
                 targetElement = targetElement->next;
             }
     
             // Insere no elemento na lista, colocando-o entre o elemento anterior
             // e o atual elemento no índice especificado.
-            previousElement->next = newElement;
             newElement->next = targetElement;
+            newElement->previous = targetElement->previous;
+
+            targetElement->previous->next = newElement;
+            targetElement->previous = newElement;
         }
 
         /**
@@ -175,8 +248,8 @@ template <typename ElementType> class LinkedList {
 
             // Obtém o elemento no índice especificado.
             struct LinkedElement<ElementType> *targetElement = firstElement;
-            
-            for (int i = 0; i < index && targetElement->next != NULL; i++) {
+
+            for (int i = 0; i < index; i++) {
                 targetElement = targetElement->next;
             }
 
@@ -196,9 +269,13 @@ template <typename ElementType> class LinkedList {
             length--;
 
             // Caso solicitado a remoção do elemento no primeiro índice, será necessário
-            // apenas fazê-lo apontar para o elemento da segunda posição e trocar o objeto.
+            // apenas vincular o segundo elemento com o último elemento. Após isso, 
+            // o primeiro elemento da lista será o segundo elemento.
             if (index == BEGIN) {
                 struct LinkedElement<ElementType> *targetElement = firstElement;
+
+                lastElement->next = targetElement->next;
+                targetElement->next->previous = lastElement;
 
                 firstElement = targetElement->next;
                 ElementType content = targetElement->content;
@@ -207,17 +284,30 @@ template <typename ElementType> class LinkedList {
                 return content;
             }
 
+            // Caso solicitado a remoção do último elemento da lista, será necessário
+            // apenas vincular o penúltimo elemento com o primeiro elemento. Após isso,
+            // o penúltimo elemento da lista será o último elemento.
+            if (index == length - 1) {
+                struct LinkedElement<ElementType> *targetElement = lastElement;
+
+                targetElement->previous->next = firstElement;
+                firstElement->previous = targetElement->previous;
+
+                ElementType content = targetElement->content;
+
+                delete targetElement;
+                return content;
+            }
+
             // Obtém o elemento no índice especificado.
-            struct LinkedElement<ElementType> *previousElement = NULL;
             struct LinkedElement<ElementType> *targetElement = firstElement;
 
-            while (--index >= 0) {
-                previousElement = targetElement;
+            for (int i = 0; i < index; i++) {
                 targetElement = targetElement->next;
             }
 
             // Conecta o elemento anterior com o sucessor do elemento alvo.
-            previousElement->next = targetElement->next;
+            targetElement->previous->next = targetElement->next;
             ElementType content = targetElement->content;
 
             delete targetElement;
