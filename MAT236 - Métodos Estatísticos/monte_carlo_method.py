cf59a3572b7afbb1e7a2e1ff402b3a15dffdee45
Jean Loui Bernard <45553309+JeanExtreme002@users.noreply.github.com>
2024-02-11 01:36:46 -0300

    plot the function and generated points with matplotlib

--- MAT236 - Métodos Estatísticos/monte_carlo_method.py
+++ MAT236 - Métodos Estatísticos/monte_carlo_method.py
@@ -26,34 +26,64 @@ class Area:
 def monte_carlo_method(area, width, height, n_samples = 10**6):
     """
     Calculate the size of an area by applying the Monte Carlo method.
+
+    Returns the size of the area and the generated points [out, in].
     """
     
     results = [0, 0]  # total of (not_valid_points, valid_points)
-
+    samples = [([], []), ([], [])]
+    
     for sample in range(n_samples):
         x = random.randint(0, width)
         y = random.randint(0, height)
-
-        results[int(area.check(x, y))] += 1
+        
+        if area.check(x,y):
+            samples[1][0].append(x)
+            samples[1][1].append(y)
+            results[1] += 1
+        else:
+            samples[0][0].append(x)
+            samples[0][1].append(y)
+            results[0] += 1
 
     total = results[0] + results[1]
     total_area = width * height
     
-    return total_area * (results[1] / total)
-    
+    return total_area * (results[1] / total), samples
+
 
 if __name__ == "__main__":
-    screen = 200, 200
+    from matplotlib import pyplot as plt
+    import numpy as np
 
+    # Bounds of the area.
+    screen = 200, 200
     f1 = lambda x, y: math.sin(x/50)*50 + 50 >= y
     f2 = lambda x, y: math.sin(x/50)*50 * -1 + 50 <= y
 
+    # Functions to draw on matplotlib.
+    f_1 = lambda x: np.sin(x/50)*50 + 50
+    f_2 = lambda x: np.sin(x/50)*50 * -1 + 50
+
+    # Number of samples.
+    n_samples = 10 ** 6
+
+    # Creating area...
     area = Area()
     area.add_bound(f1)
     area.add_bound(f2)
 
-    n_samples = 10 ** 6
+    # Calculating...
+    area, samples = monte_carlo_method(area, *screen, n_samples)
+    print("Area =", area)
 
-    print("Testing...")
+    # Plotting...
+    x = np.linspace(0, screen[0], screen[0])
+    
+    plt.plot(x, f_1(x), color='black')
+    plt.plot(x, f_2(x), color='black')
 
-    print("Area =", monte_carlo_method(area, *screen, n_samples))
+    plt.scatter(samples[0][0], samples[0][1], color = 'orange')
+    plt.scatter(samples[1][0], samples[1][1], color = 'green')
+    
+    plt.show()
